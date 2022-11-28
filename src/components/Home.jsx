d4dbf8df800b360220ef7c1b841be0d4aa8dcf8e
Hellsh4rk <yahir.lope@icloud.com>
2022-11-27 18:22:50 -0600

    add a new home feature

---
+++ src/components/Home.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div>
+            <div className="header">
+                <div className="vectorHome">
+                    <img src="../images/VectorHome.png" alt="VectorHome" />
+                </div>
+                <div className="headerText">
+                    <p className="headerText1">How we work</p>
+                    <p className="headerText2">Services</p>
+                    <p className="headerText3">Free Quote</p>
+                    <p className="headerText4">Contact</p>
+                </div>
+            </div>
+
+        <div className="contentText">
+            <h1>What items to store?</h1>
+        </div>
+        <div className="contentText2">
+            <p>Select which items you wish to store before moving to your new home. We'll keep 'em safe!</p>
+        </div>
+    </div>
+    );
+};
+
+export default Home;
